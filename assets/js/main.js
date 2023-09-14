1dc578118a99d17d9e5671ca5c3f990e985944a5
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-09-14 15:34:24 +0200

    update index & main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// 1 - Jquerry
 (function($) {
 
 	var	$window = $(window),
@@ -26,3 +27,28 @@
 		});
 
 })(jQuery);
+
+// 2- Javascript
+const inputName = document.querySelector('#name');
+const inputEmail = document.querySelector('#email');
+const inputSubject = document.querySelector('#subject');
+const inputMessage = document.querySelector('#message');
+const btnSendMessage = document.querySelector('#btn_send_message');
+
+btnSendMessage.addEventListener('click', function(e){
+	e.preventDefault();
+	const message = `
+	Message de ${inputName.value}, mail : ${inputEmail.value},
+	${inputMessage.value}
+	`;
+	console.log(`inputName : ${inputName.value}, 
+				inputEmail : ${inputEmail.value},
+				inputSubject : ${inputSubject.value},
+				inputMessage : ${inputMessage.value},
+				message : ${message}
+	`);
+
+	window.open(`mailto:nicolas.gautier35135@gmail.com?subject=${inputSubject.value}&body=${message}`)
+})
+
+
